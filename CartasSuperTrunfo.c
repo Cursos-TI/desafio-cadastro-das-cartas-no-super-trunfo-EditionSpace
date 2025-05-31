e968a7f4361aaea236a6b4c18efa1cdf1889e4a3
EditionSpace <acountschool123@gmail.com>
2025-05-31 04:31:08 +0000

    determinei as variáveis e seus valores

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -3,6 +3,21 @@
 int main() { 
     printf("Super trunfo!\n");
 
+    char letra = 'A';
+    char código[8];
+    char nome[20] = "Gramado";
+    int população;
+    float área;
+    float pib;
+    int pontos;
+
+
+    
+
+
+
+
+
    
 
 
